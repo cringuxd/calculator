66e52dbf6e29516023f54aee9e4cb50a588be999
cringuxd <193437229+cringuxd@users.noreply.github.com>
2025-03-13 12:42:29 -0500

    Implement full textbox functionality
    Included with this is some partially functioning code for the + button and clear button, although the calc functions are currently buggy.

--- script.js
+++ script.js
@@ -1,7 +1,73 @@
 //Operation Variables
-let number1 = 0;
+let number1 = "";
 let operator = "";
-let number2 = 0;
+let number2 = "";
+
+//Calculator Variables
+const num_button = document.querySelector(".num-button");
+const num_buttons = document.querySelectorAll(".num-button");
+const calc_buttons = document.querySelectorAll(".calc-button");
+const text = document.querySelector(".textbox");
+
+//Button Functions
+
+/*Number Buttons*/
+for(let i = 0; i < num_buttons.length; i++) {
+    num_buttons[i].onclick = () => {
+        if(text.textContent == "0") {
+            text.textContent = num_buttons[i].textContent;
+        }
+        else {
+            text.textContent += num_buttons[i].textContent;
+        }
+    };
+}
+
+/*Function Buttons*/
+
+//Arithmetic Cases
+/**Case 1: First number (num1/num2 empty): populate first number and operator variables
+   Case 2: Second number (num2 empty): populate second number -> evaluate -> store result in first num var. -> set second num var. to ""
+   Case 3:  **/
+// +
+calc_buttons[0].onclick = () => {
+    
+    if(number1 == "" && number2 == "") {
+        number1 = text.textContent;
+        operator = '+';
+        text.textContent = "";
+    }
+    else if(number2 == "") {
+        number2 = text.textContent;
+        number1 = (operate(Number(number1), operator, Number(number2))).toString();
+        number2 = "";
+        text.textContent = number1;
+    }
+        
+}
+// -
+calc_buttons[1].onclick = () => {
+    
+}
+// *
+calc_buttons[2].onclick = () => {
+    
+}
+// /
+calc_buttons[3].onclick = () => {
+    
+}
+// =
+calc_buttons[4].onclick = () => {
+    
+}
+// CLEAR
+calc_buttons[5].onclick = () => {
+    number1 = "";
+    operator = "";
+    number2 = "";
+    text.textContent = "0";
+}
 
 //Calculator Functions (Add/Sub/Mult/Div)
 function add(num1,num2) {
@@ -39,5 +105,7 @@ function operate(num1,op,num2) {
         default:
             return "ERROR";
     }
+    operator = "";
     return finalVal;
 }
+
